3663ef5148039e19326626fb7acb9bfb0069e943
Michel Weststrate <mweststrate@gmail.com>
2017-02-16 14:03:01 +0100

    Fixed test

--- src/components/App.test.js
+++ src/components/App.test.js
@@ -1,12 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
+import { Provider } from 'mobx-react'
+
 import BookStore from "../stores/BookStore"
+import CartStore from "../stores/CartStore"
 
 it('renders without crashing', () => {
+  const bookStore = new BookStore()
+  const cartStore = new CartStore()
+
   const div = document.createElement('div')
   ReactDOM.render(
-    <App bookStore={new BookStore()} />,
+    <Provider bookStore={bookStore} cartStore={cartStore}>
+      <App />
+    </Provider>,
     div
   )
 })
